2e93deae7ff00d8c3e746ab61eb4fe75cdc4a746
Henning Sauter <henning.sauter@hu-berlin.de>
2024-12-04 11:21:23 +0100

    added puzzle solution day 4

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Advent of code
+
 Problems list:
+
 - [Day 1: Historian Hysteria](https://github.com/Fate6174/advent-of-code-2024/blob/main/src/01.py)
 - [Day 2: Red-Nosed Reports](https://github.com/Fate6174/advent-of-code-2024/blob/main/src/02.py)
 - [Day 3: Mull It Over](https://github.com/Fate6174/advent-of-code-2024/blob/main/src/03.py)
+- [Day 4: Ceres Search](https://github.com/Fate6174/advent-of-code-2024/blob/main/src/04.py)
 
 Created via: [advent-of-code-setup](https://github.com/tomfran/advent-of-code-setup)
---
+++ src/04.py
@@ -0,0 +1,46 @@
+from utils.api import get_input
+
+input_str = get_input(4)
+
+# WRITE YOUR SOLUTION HERE
+input_matrix = input_str.splitlines()
+m, n = len(input_matrix), len(input_matrix[0])
+
+# part 1
+res = 0
+for i in range(m):
+    for j in range(n):
+        if input_matrix[i][j] == 'X':
+            l = []
+            if j > 2:
+                l += [[input_matrix[i][j-idx_shift] for idx_shift in [1,2,3]]]
+                if i > 2:
+                    l += [[input_matrix[i-idx_shift][j-idx_shift] for idx_shift in [1,2,3]]]
+                if i < m - 2 - 1:
+                    l += [[input_matrix[i+idx_shift][j-idx_shift] for idx_shift in [1,2,3]]]
+            if i > 2:
+                l += [[input_matrix[i-idx_shift][j] for idx_shift in [1,2,3]]]
+            if i < m - 2 - 1:
+                l += [[input_matrix[i+idx_shift][j] for idx_shift in [1,2,3]]]
+            if j < n - 2 - 1:
+                l += [[input_matrix[i][j+idx_shift] for idx_shift in [1,2,3]]]
+                if i > 2:
+                    l += [[input_matrix[i-idx_shift][j+idx_shift] for idx_shift in [1,2,3]]]
+                if i < m - 2 - 1:
+                    l += [[input_matrix[i+idx_shift][j+idx_shift] for idx_shift in [1,2,3]]]
+            res += sum([1 if ''.join(item) == 'MAS' else 0 for item in l])
+
+print(f'part1: {res}')
+
+# part 2
+res = 0
+for i in range(1,m-1):
+    for j in range(1,n-1):
+        if input_matrix[i][j] == 'A':
+            l = [
+                [input_matrix[i-idx_shift][j-idx_shift] for idx_shift in [-1,0,1]],
+                [input_matrix[i+idx_shift][j-idx_shift] for idx_shift in [-1,0,1]]
+            ]
+            res += 1 if all([''.join(item) == 'MAS' or ''.join(item) == 'SAM' for item in l]) else 0
+
+print(f'part2: {res}')
