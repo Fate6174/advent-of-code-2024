8079c9d74c77ded39753f169528887c39f01bf1e
Henning Sauter <sauter@math.hu-berlin.de>
2024-12-05 15:23:35 +0100

    added puzzle day 05

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ Problems list:
 - [Day 2: Red-Nosed Reports](https://github.com/Fate6174/advent-of-code-2024/blob/main/src/02.py)
 - [Day 3: Mull It Over](https://github.com/Fate6174/advent-of-code-2024/blob/main/src/03.py)
 - [Day 4: Ceres Search](https://github.com/Fate6174/advent-of-code-2024/blob/main/src/04.py)
+- [Day 5: Print Queue](https://github.com/Fate6174/advent-of-code-2024/blob/main/src/05.py)
 
 Created via: [advent-of-code-setup](https://github.com/tomfran/advent-of-code-setup)
--- src/05.py
+++ src/05.py
@@ -1,62 +1,43 @@
 from utils.api import get_input
 
-input_str = """47|53
-97|13
-97|61
-97|47
-75|29
-61|13
-75|53
-29|13
-97|29
-53|29
-61|53
-97|53
-61|29
-47|13
-75|47
-97|75
-47|61
-75|61
-47|29
-75|13
-53|13
-
-75,47,61,53,29
-97,61,53,29,13
-75,29,13
-75,97,47,61,53
-61,13,29
-97,13,75,29,47"""
-# input_str = get_input(5)
+input_str = get_input(5)
 
 # WRITE YOUR SOLUTION HERE
 sep_idx = input_str.find('\n\n')
 rules = [(int(item[:2]), int(item[3:])) for item in input_str[:sep_idx].splitlines()]
 updates = [[int(x) for x in item.split(',')] for item in input_str[sep_idx+2:].splitlines()]
 
-def sort_rules(l : list[tuple[int,int]]) -> list[int]:
-    
-
-print(rules)
-# print(updates)
+def sort(rules : list[tuple[int,int]]) -> list[int]:
+    out = []
+    while rules != []:
+        if len(rules) == 1:
+            out += [rules[0][0], rules[0][1]]
+            rules = []
+        else:
+            smallest_numbers = {a for a,b in rules} - {b for a,b in rules}
+            if len(smallest_numbers) != 1:
+                raise RuntimeError(f'No unique minimum in {rules}.')
+            smallest_number = smallest_numbers.pop()
+            out += [smallest_number]
+            rules = [item for item in rules if item[0] != smallest_number]
+    return out
 
 # part 1
-def match_safe(l : list[int]) -> int:
-    for i in range(len(l)):
-        for j in range(i):
-            for rule in rules:
-                a, b = rule
-                if l[j] == b and l[i] == a:
-                    return 0
-        for j in range(i+1,len(l)):
-            for rule in rules:
-                a, b = rule
-                if l[j] == a and l[i] == b:
-                    return 0
-    return l[len(l) // 2]
+res = 0
+for update in updates:
+    relevant_rules = [item for item in rules if item[0] in update and item[1] in update]
+    local_order = sort(relevant_rules)
+    if update == local_order:
+        res += update[len(update) // 2]
 
-res = sum([match_safe(update) for update in updates])
 print(f'part1: {res}')
 
 # part 2
+res = 0
+for update in updates:
+    relevant_rules = [item for item in rules if item[0] in update and item[1] in update]
+    local_order = sort(relevant_rules)
+    if update != local_order:
+        res += local_order[len(local_order) // 2]
+
+print(f'part2: {res}')
